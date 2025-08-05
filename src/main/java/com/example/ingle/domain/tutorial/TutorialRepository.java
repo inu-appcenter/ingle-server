ca066e42afb420e926a61e4bc57ec0450fade9ac
da1nda2n <da1nda2n@gmail.com>
2025-08-05 10:31:33 +0900

    🔧 chore : Unused import 제거 - #2

--- src/main/java/com/example/ingle/domain/tutorial/TutorialRepository.java
+++ src/main/java/com/example/ingle/domain/tutorial/TutorialRepository.java
@@ -1,6 +1,5 @@
 package com.example.ingle.domain.tutorial;
 
-import com.example.ingle.domain.memberreward.MemberReward;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
