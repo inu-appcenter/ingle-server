e53f73910d7a3ac6968324254e7fb89c56c792d7
d6nggyun <d6nggyun@gmail.com>
2025-09-17 21:52:59 +0900

    ♻️ refactor: building 단순 조회 시 객체 대신 응답에 필요한 필드 값만 조회하도록 수정

--- src/main/java/com/example/ingle/domain/building/repository/BuildingRepository.java
+++ src/main/java/com/example/ingle/domain/building/repository/BuildingRepository.java
@@ -1,6 +1,7 @@
 package com.example.ingle.domain.building.repository;
 
 import com.example.ingle.domain.building.domain.Building;
+import com.example.ingle.domain.building.dto.res.BuildingResponse;
 import com.example.ingle.domain.building.enums.BuildingCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,17 +12,26 @@ import java.util.List;
 public interface BuildingRepository extends JpaRepository<Building,Long> {
 
     @Query("""
-        SELECT m FROM Building m
+        SELECT new com.example.ingle.domain.building.dto.res.BuildingResponse
+            (m.id, m.buildingName, m.latitude, m.longitude, m.buildingCategory)
+        FROM Building m
         WHERE m.latitude BETWEEN :minLat AND :maxLat
         AND m.longitude BETWEEN :minLng AND :maxLng
         AND (:category IS NULL OR m.buildingCategory = :category)
     """)
-    List<Building> findBuildingsInBounds(
+    List<BuildingResponse> findBuildingsInBounds(
             @Param("minLat") double minLat,
             @Param("maxLat") double maxLat,
             @Param("minLng") double minLng,
             @Param("maxLng") double maxLng,
             @Param("category") BuildingCategory category);
 
-    List<Building> findByBuildingNameContainingOrderByCreatedAtDesc(String buildingName);
+    @Query("""
+        SELECT new com.example.ingle.domain.building.dto.res.BuildingResponse
+            (m.id, m.buildingName, m.latitude, m.longitude, m.buildingCategory)
+        FROM Building m
+        WHERE m.buildingName LIKE CONCAT('%', :buildingName, '%')
+        ORDER BY m.createdAt DESC
+    """)
+    List<BuildingResponse> findByBuildingNameContainingOrderByCreatedAtDesc(String buildingName);
 }
