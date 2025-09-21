dd44048774fd7022101404f854e6bad76e5b190e
d6nggyun <d6nggyun@gmail.com>
2025-09-21 19:16:20 +0900

    ♻️ refactor: MySQLConfig 코드 정리

--- src/main/java/com/example/ingle/global/config/MysqlDataSourceProperties.java
+++ src/main/java/com/example/ingle/global/config/MysqlDataSourceProperties.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
 @ConfigurationProperties(prefix = "spring.datasource")
-public class MysqlDataSourceProperties {
+public class MysqlDataSourceProperties { // application.yml 에 정의한 MySQL 데이터베이스 접속 정보를 객체로 매핑하기 위한 전용 설정 클래스
 
     private String url;
     private String username;
