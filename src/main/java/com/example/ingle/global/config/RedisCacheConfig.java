6359dd65cdd5635705154c284398d974ca3d5acf
d6nggyun <d6nggyun@gmail.com>
2025-12-29 20:56:23 +0900

    🌎 ci: prod 배포에 수동 배포 추가

--- src/main/java/com/example/ingle/global/config/RedisCacheConfig.java
+++ src/main/java/com/example/ingle/global/config/RedisCacheConfig.java
@@ -31,10 +31,11 @@ public class RedisCacheConfig {
                 .registerModule(new Jdk8Module()) // Optional 지원
                 .registerModule(new JavaTimeModule()); // LocalDateTime 등 지원
 
+        // Redis 캐싱 시 JSON을 원래 객체 타입으로 되살리기 위한 Type Metadata 삽입 기능
         objectMapper.activateDefaultTyping(
-                LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL,
-                JsonTypeInfo.As.PROPERTY
+                LaissezFaireSubTypeValidator.instance, // 어떤 타입에 대해서 타입 정보를 허용할지를 검사하는 Validator -> 모든 타입
+                ObjectMapper.DefaultTyping.NON_FINAL, // 어떤 클래스에 대해 타입 정보를 붙일 것인지 -> final이 아닌 모든 타입
+                JsonTypeInfo.As.PROPERTY // 타입 정보를 JSON 어디에 기록할 것인가 -> @class 속성을 하나 추가
         );
 
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
